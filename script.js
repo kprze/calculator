a1b31b4b2ac8cbdbff189d00e5e2c7169c2ad22a
kprze <kacperej@gmail.com>
2024-02-05 11:52:07 +0000

    add basic functionality

---
+++ script.js
@@ -0,0 +1,109 @@
+let buttonValues = {
+    number: [0,1,2,3,4,5,6,7,8,9],
+    operator: ['+','-','*','/'],
+    point: ".",
+    equal: "="
+}
+
+let buttons = document.querySelectorAll('button');
+let userInput = [];
+let value1 = null;
+let value2 = null;
+let operator = null;
+let result = null;
+
+getInputValue();
+
+function getInputValue(){
+    buttons.forEach(function(button){
+        button.addEventListener("click", function() {
+            console.log(processInputValue(this.id));
+        });
+    });
+};
+
+function processInputValue(inputValue){
+    if(buttonValues.number.includes(parseInt(inputValue))){
+        userInput.push(parseInt(inputValue));
+        return userInput;
+    }
+
+    if(buttonValues.operator.includes(inputValue)){
+        if(value1 == null && result == null){
+            value1 = parseInt(userInput.join(''));
+            userInput.length = 0;
+            return operator = inputValue;
+        } else if (value1 == null & result != null){
+            return operator = inputValue;
+        };
+    };
+
+    if(inputValue == buttonValues.equal){
+        value2 = parseInt(userInput.join(''));
+        userInput.length = 0;
+
+        if(value1 == null && result != null){
+            switch(operator){
+                case '+': addition(value1,value2)
+                break;
+                case '-': subtraction(value1,value2)
+                break;
+                case '*': multiplication(value1,value2)
+                break;
+                case '/': division(value1,value2)
+                break;
+            };
+        };
+
+        if(value1 != null && result != null){
+            switch(operator){
+                case '+': addition(value1,value2)
+                break;
+                case '-': subtraction(value1,value2)
+                break;
+                case '*': multiplication(value1,value2)
+                break;
+                case '/': division(value1,value2)
+                break;
+            };
+        };
+
+        value1 = null;
+        value2 = null;
+        operator = null;
+        return result;
+    };
+
+}
+
+function addition(value1, value2){
+    if (result != null){
+        return result = result + value2;
+    } else {
+        return result = value1 + value2;
+    };
+}
+
+function subtraction(value1, value2){
+    if (result != null){
+        return result = result - value2;
+    } else {
+        return result = value1 - value2;
+    };
+}
+
+function multiplication(value1, value2){
+    if (result != null){
+        return result = result * value2;
+    } else {
+        return result = value1 * value2;
+    };
+}
+
+function division(value1, value2){
+    if (result != null){
+        return result = result / value2;
+    } else {
+        return result = value1 / value2;
+    };
+}
