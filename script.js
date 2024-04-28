05c4e459f5a714871d25cfce9230473002a837e1
kprze <kacperej@gmail.com>
2024-04-28 17:23:13 +0100

    Update display and operand functions

--- script.js
+++ script.js
@@ -1,66 +1,80 @@
-let firstVal = null;
-let secondVal = null;
-let firstOp = null;
-let secondOp = null;
+let value1 = null;
+let value2 = null;
+let operator1 = null;
+let operator2 = null;
 let displayValue = null;
-let operators = ['+', '-', '*', '/'];
+let result = null;
 
 let buttons = document.querySelectorAll('button');
 let display = document.getElementById('display');
 
 getInputValue();
 
-
 function getInputValue(){
     buttons.forEach(function(button){
         button.addEventListener("click", function() {
-            if(this.id === 'operand'){
-                updateDisplay(this.value)
-            } else if(this.id === 'operator'){
-
+            if(this.className == 'operand'){
+                // updateDisplay(this.value);
+                processOperand(this.value)
+            } else if(this.className == 'operator'){
+                processOperator(this.value);
+            } else if(this.className == 'equals'){
+                processEquals(this.value);
             }
         });
     });
 };
 
 function updateDisplay(userInput){
-    if(displayValue == null){
-        displayValue = userInput
+    display.textContent = userInput;
+}
+
+function processOperator(userInput){
+    if(operator1 == null){
+        operator1 = userInput;
+    } else if(operator2 == null){
+        operator2 = userInput;
+    }
+}
+
+function processOperand(userInput){
+    if(operator1 == null && operator2 == null){
+        if(value1 == null){
+            value1 = userInput;
+        } else if(value1 != null){
+            value1 += userInput;
+        }
+        updateDisplay(value1);
     } else {
-        displayValue = displayValue + userInput
-    }
-    display.textContent = displayValue;
-}
-
-function processInputValue(inputValue){
-
-}
-
-function operate(firstVal, secondVal, currOperator){
-    switch(currOperator){
-        case '+': add(firstVal, secondVal)
-        break;
-        case '-': subtract(firstVal, secondVal)
-        break;
-        case '*': multiply(firstVal, secondVal)
-        break;
-        case '/': divide(firstVal, secondVal)
-        break;
+        if(value2 == null){
+            value2 = userInput;
+        } else if(value2 != null){
+            value2 += userInput;
+        }
+        updateDisplay(value2); 
     }
 }
 
-function add(firstVal, secondVal){
-    return firstVal + secondVal;
+function processEquals(userInput){
+
 }
 
-function subtract(firstVal, secondVal){
-    return firstVal - secondVal;
+function operate(value1, value2, operator){
+
 }
 
-function multiply(firstVal, secondVal){
-    return firstVal * secondVal;
+function add(value1, value2){
+    return value1 + value2;
 }
 
-function divide(firstVal, secondVal){
-    return firstVal / secondVal;
+function subtract(value1, value2){
+    return value1 - value2;
+}
+
+function multiply(value1, value2){
+    return value1 * value2;
+}
+
+function divide(value1, value2){
+    return value1 / value2;
 }
